a8ea0a5b85cd530128895bc04da4f676eba81c52
Socheat Sok <alex@socheat.net>
2020-09-18 21:28:53 +0700

    feat: add new remote install.sh

---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+# Default settings
+ZSH_RC=~/.zshrc
+TERMUX_DOTFILE=${TERMUX_DOTFILE:-~/.termux}
+REPO=${REPO:-socheatsok78/.termux}
+REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+BRANCH=${BRANCH:-master}
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"Error: $@"${RESET} >&2
+}
+
+underline() {
+	echo "$(printf '\033[4m')$@$(printf '\033[24m')"
+}
+
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
+}
+
+setup_dotfile() {
+	# Prevent the cloned repository from having insecure permissions. Failing to do
+	# so causes compinit() calls to fail with "command not found: compdef" errors
+	# for users with insecure umasks (e.g., "002", allowing group writability). Note
+	# that this will be ignored under Cygwin by default, as Windows ACLs take
+	# precedence over umasks except for filesystems mounted with option "noacl".
+	umask g-w,o-w
+
+	echo "${BLUE}Cloning .termux...${RESET}"
+
+    command_exists git || {
+		error "git is not installed"
+		exit 1
+	}
+
+	git clone -c core.eol=lf -c core.autocrlf=false \
+		-c fsck.zeroPaddedFilemode=ignore \
+		-c fetch.fsck.zeroPaddedFilemode=ignore \
+		-c receive.fsck.zeroPaddedFilemode=ignore \
+		--depth=1 --branch "$BRANCH" "$REMOTE" "$TERMUX_DOTFILE" || {
+		error "git clone of .termux repo failed"
+		exit 1
+	}
+
+	echo
+}
+
+main() {
+	setup_color
+
+	if ! command_exists zsh; then
+		echo "${YELLOW}Zsh is not installed.${RESET} Please install zsh first."
+		exit 1
+	fi
+
+	setup_dotfile
+}
