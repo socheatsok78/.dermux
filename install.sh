c91a091f662f7b48f3301cd82fbfbd1c30f9a7f9
Socheat Sok <alex@socheat.net>
2020-09-18 20:53:47 +0700

    feat: add install.sh script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+export CHSH=yes
+export RUNZSH=no
+
+echo " ---> installing curl"
+pkg install -y libcurl curl
+
+echo " ---> installing zsh"
+pkg install -y zsh
+
+echo " ---> install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo " ---> Installation complated!"
